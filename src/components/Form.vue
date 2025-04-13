a1bdbbb915ada0e73458754f0d072e339fd1e98c
wczhuo <wczhuo@qq.com>
2025-04-13 17:27:59 +0800

    Form组件，动态监听字段数值变化

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {h, ref, useTemplateRef} from 'vue';
+import {h, ref, useTemplateRef, watch, computed} from 'vue';
 import {Spin, Form, FormItem, Input} from 'ant-design-vue';
 
 const componentsMap: { [key: string]: any } = {
@@ -7,13 +7,22 @@ const componentsMap: { [key: string]: any } = {
 };
 
 const formState = ref(<Record<string, any>>{});
+const rulesState = ref(<Record<string, any>>{});
 const formApi = useTemplateRef('from');
+
+interface Dependencies {
+  triggerFields: Array<string>;
+  required: boolean | Function;
+  show: boolean | Function;
+}
 
 interface FormSchema {
   label?: string;
   fieldName?: string | undefined | any;
   component?: string;
   componentProps?: object;
+  rules?: string;
+  dependencies?: Dependencies;
 }
 
 const props = defineProps({
@@ -29,29 +38,76 @@ const props = defineProps({
     type: Object,
     default: () => ({
       formItemClass: 'col-span-1',
-      wrapperCol: { span: 16 },
-      labelCol: { span: 8 }
+      wrapperCol: {span: 16},
+      labelCol: {span: 8}
     }),
   },
   wrapperClass: {
     type: String,
-    default: 'grid-cols-2',
+    default: 'grid-cols-2 gap-x-4',
   },
   wrapperCol: {
     type: Object,
-    default: { span: 16 },
+    default: {span: 16},
   },
   labelCol: {
     type: Object,
-    default: { span: 6 },
+    default: {span: 6},
   }
 });
+
+// 鉴定数据变化
+watch(
+    () => ({...formState.value}),
+    (newState: any, oldState: any) => {
+      // 更新rulesState
+      console.log(`formState变化:`, oldState, ' → ', newState);
+      props.schema.forEach(item => {
+        let isChange = false;
+        if (item?.dependencies?.triggerFields) {
+          item?.dependencies?.triggerFields.forEach(field => {
+            isChange = newState[field] !== oldState[field];
+            console.log(item.fieldName, 'isChange', isChange, field, oldState[field], newState[field]);
+          });
+        }
+        console.log(item.fieldName, 'isChange', isChange);
+        // 如果字段值发生了变化，则重新生成规格
+        if (isChange) {
+          rulesState.value[item.fieldName] = {
+            message: item.rules == 'selectRequired' ? `请选择${item.label}` : `请输入${item.label}`,
+            required: (item?.dependencies?.required instanceof Function) ? (item?.dependencies?.required(newState)) : (item?.dependencies?.required === true ? true : !!item.rules),
+          };
+        }
+        console.log('rulesState', rulesState);
+      });
+    },
+    {deep: true}
+)
+
+// 初始化rulesState
+props.schema.forEach(item => {
+  rulesState.value[item.fieldName] = {
+    message: item.rules == 'selectRequired' ? `请选择${item.label}` : `请输入${item.label}`,
+    required: (item?.dependencies?.required instanceof Function) ? (item?.dependencies?.required(formState)) : (item?.dependencies?.required === true ? true : !!item.rules),
+  };
+});
+console.log('rulesState', rulesState);
 
 const formItemClass = (item: any, __index: any) => {
   return item?.formItemClass ? item?.formItemClass : props.commonConfig.formItemClass;
 }
-const formItemRules = (__item: any, __index: any) => {
-  return [];
+const formItemRules: any = (__item: any, __index: any) => {
+  return {
+    message: '12123',
+    required: true,
+  };
+  // computed无效
+  // return computed(() =>{
+  //   return {
+  //     message: '12123',
+  //     required: true,
+  //   };
+  // });
 }
 const formItemComponent = (item: any, __index: any) => {
   // return '123123';
@@ -79,17 +135,19 @@ defineExpose({setValues, resetForm, setFieldValue, formApi});
 <template>
   <div class="form-container">
     <Spin :spinning="loading">
-      <Form ref="form" :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol" :class="wrapperClass" class="grid">
-          <template v-for="(item, index) in schema">
-            <FormItem :class="formItemClass(item, index)" :rules="formItemRules(item, index)" :label="(item as any).label" :label-col="formItemLabelCol(item, index)" :wrapper-col="formItemWrapperCol(item, index)">
-<!--              <component :is="formItemComponent(item, index)"/>-->
-              <!-- 动态渲染组件 -->
-              <component
-                  :is="formItemComponent(item, index)"
-                  v-model:value="formState[item.fieldName]"
-                  v-bind="item.componentProps"
-              />
-            </FormItem>
+      <Form ref="form" :model="formState" :label-col="labelCol" :wrapper-col="wrapperCol" :class="wrapperClass"
+            class="grid">
+        <template v-for="(item, index) in schema">
+          <FormItem :class="formItemClass(item, index)" :rules="rulesState[item.fieldName]" :label="(item as any).label"
+                    :label-col="formItemLabelCol(item, index)" :wrapper-col="formItemWrapperCol(item, index)">
+            <!--              <component :is="formItemComponent(item, index)"/>-->
+            <!-- 动态渲染组件 -->
+            <component
+                :is="formItemComponent(item, index)"
+                v-model:value="formState[item.fieldName]"
+                v-bind="item.componentProps"
+            />
+          </FormItem>
         </template>
       </Form>
     </Spin>
--- src/views/dashboard/analytics/index.vue
+++ src/views/dashboard/analytics/index.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import Form from '@/components/Form.vue';
+
 const schema: any = [
   {
     fieldName: 'id',
@@ -9,24 +10,45 @@ const schema: any = [
   {
     fieldName: 'name',
     label: 'name',
+    rules: 'required',
     component: 'Input',
-    slot: (row: any)=>{
+    slot: (row: any) => {
       return row.name;
     },
-    wrapperCol: { span: 8 },
-    labelCol: { span: 4 }
+    wrapperCol: {span: 8},
+    labelCol: {span: 4}
+  },
+  {
+    fieldName: 'type',
+    label: 'type',
+    component: 'Input',
+    slot: (row: any) => {
+      return row.name;
+    },
+    wrapperCol: {span: 8},
+    labelCol: {span: 4}
   },
   {
     fieldName: 'title',
     label: 'title',
     component: 'Input',
     slot: '',
+    dependencies: {
+      triggerFields: ['type'],
+      required: (values: any) => {
+        return (['full_reduce', 'full_discount', 'free_shipping'].indexOf(values.type) >= 0);
+      },
+      show: (values: any) => {
+        return (['full_reduce', 'full_discount', 'free_shipping'].indexOf(values.type) >= 0);
+      },
+    },
   },
+
 ];
 </script>
 
 <template>
-<Form :schema="schema"></Form>
+  <Form :schema="schema"></Form>
 </template>
 
 <style scoped>
